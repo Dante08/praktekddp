1a9c178d8865c65a1ca780acc396be8fe90ea7f2
wanda agustiansyah r <wandatamvan@gmail.com>
2018-09-17 11:03:26 +0700

    pertemuan ketiga

---
+++ pertemuan ketiga/variable.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main () {
+	int a=5;
+	int b=20;
+	int c=14;
+	cout <<"\t 1. nilai dari a adalah " <<a<<endl;
+	cout <<"\t 2. nilai dari b adalah " <<b<<endl;
+	cout <<"\t 3. nilai dari c adalah " <<c<<endl;
+	
+return 0;
+}
---
+++ pertemuan ketiga/variable_coba.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+	char nama_lengkap[30];
+	char nama_sekolah[30];
+	cout<<" nama lengkap adalah : ";
+	cin.get(nama_lengkap, 30);
+	cout<<" nama sekolah anda : ";
+	cin>>nama_sekolah;
+	cout<<endl;
+	cout<<" nama lengkap adalah : "<<nama_lengkap<<endl;
+	cout<<" nama sekolah adalah : "<<nama_sekolah;
+	
+return 0;
+}
