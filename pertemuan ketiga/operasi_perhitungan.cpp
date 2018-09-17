b7bdb0d3ac610bb3cdc12d8e09e286b42ea269ff
wanda agustiansyah r <wandatamvan@gmail.com>
2018-09-17 11:01:04 +0700

    pertemuan ketiga
     senin 17 september 2018 : materi variable, blabla

---
+++ pertemuan ketiga/operasi_perhitungan.cpp
@@ -0,0 +1,18 @@
+\* senin 17 september 2018 : materi variable, blabla *\
+#include <iostream>
+using namespace std;
+int main() {
+	int panjang, lebar, tinggi, luas;
+	cout<<"\t 1. panjang adalah : ";
+	cin>>panjang;
+	cout<<"\t 2. lebar adalah : ";
+	cin>>lebar;
+	cout<<"\t 3. tinggi adalah : ";
+	cin>>tinggi;
+	
+	luas = panjang*lebar*tinggi;
+	cout<<"\t 4. luas adalah : " <<luas<<endl;
+	
+return 0;
+}
+
