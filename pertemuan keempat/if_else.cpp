3ec94642fd4e6dc5ac1b04c62baa6a5eb8b33271
wanda agustiansyah r <wandatamvan@gmail.com>
2018-09-24 11:07:45 +0700

    pertemuan keempat
    penggunaan if else

---
+++ pertemuan keempat/if_else.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+int a;
+cout << "menentukan bilangan genap dan ganjil = ";
+cin >> a;
+	if (a%2==0){
+		cout << "bilangan genap"<<endl;
+	}
+	else {
+		cout << "bilangan ganjil"<<endl;
+	}
+	
+}
